d6eb97a87d2aa68622d61ccdef7eea7eefeb5a9a
Anattapong Suktep <benzanattapong@Anattapongs-MacBook-Pro.local>
2021-03-10 11:07:45 +0700

    feat: update delete name action

--- myName/ViewController.swift
+++ myName/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
@@ -16,27 +16,47 @@ class ViewController: UIViewController {
         
         title = "Name"
         
-        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
-                let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
-                    
-                    guard let textField = alert.textFields?.first,
-                          let nameToSave = textField.text else {
-                        return
-                    }
-                    self.nameLabel.text = "Hello: \(nameToSave)"
-                }
-                
-                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-                
-                alert.addTextField()
-                alert.addAction(saveAction)
-                alert.addAction(cancelAction)
-                
-                present(alert, animated: true)
-    }
-
-    @IBAction func deleteNameButtonPressed(_ sender: Any) {
+        if nameLabel.text != "" {
+            setupAlert()
+        }
         
     }
+    
+    func setupAlert() {
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+            
+            guard let textField = alert.textFields?.first,
+                  let nameToSave = textField.text else {
+                return
+            }
+            self.nameLabel.text = "Hello: \(nameToSave)"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    @IBAction func deleteNameButtonPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Alert", message: "Are you sure you watn to delete?", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "OK", style: .destructive) { [unowned self] action in
+            self.nameLabel.text = "Your name"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(confirmAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func addNameButtonPressed(_ sender: Any) {
+        
+    }
+    
 }
 
